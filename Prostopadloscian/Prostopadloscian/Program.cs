60eb05518b6f2714ebd578475a30f635d25367d5
Jasan <127194187+Jasan5@users.noreply.github.com>
2023-04-25 20:50:10 +0200

    zadanie

---
+++ Prostopadloscian/Prostopadloscian/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prostopadloscian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostopadloscian p1 = new Prostopadloscian();
+            p1.d = 3;
+            p1.s = 4;
+            p1.w = 5;
+            Console.WriteLine(p1.objetosc());
+            Prostopadloscian p2 = new Prostopadloscian();
+            p2.d = 5;
+            p2.s = 7;
+            p2.w = 4;
+            p1.porownajObjetosc(p2);
+        }
+    }
+
+    public class Prostopadloscian
+    {
+        public float d { get; set; }
+        public float s { get; set; }
+        public float w { get; set; }
+        public float objetosc()
+        {
+            return d * s * w;
+        }
+        public void porownajObjetosc(Prostopadloscian p)
+        {
+            if (this.objetosc() > p.objetosc())
+                Console.WriteLine("Prostopadłościan pierwszy ma większą objętość niż drugi");
+            else if (this.objetosc() < p.objetosc())
+                Console.WriteLine("Prostopadłościan pierwszy ma mniejszą objętość niż drugi");
+            else
+                Console.WriteLine("Oba prostopadłościany mają jednakową objętość");
+        }
+    }
+}
---
+++ Studia/Studia/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KandydatNaStudia[] k = new KandydatNaStudia[3];
+            k[0] = new KandydatNaStudia("Kowal", 100, 86, 65);
+            k[1] = new KandydatNaStudia("Nowak", 55, 45, 90);
+            k[2] = new KandydatNaStudia("Orka", 33, 74, 24);
+            foreach (var n in k)
+            {
+                Console.WriteLine("{0} {1}", n.nazwisko, n.Punkty(n.punktyMatematyka, n.punktyInformatyka, n.punktyJezykObcy));
+            }
+        }
+    }
+
+    struct KandydatNaStudia
+    {
+        public string nazwisko;
+        public int punktyMatematyka, punktyInformatyka, punktyJezykObcy;
+        public KandydatNaStudia(string naz, int mat, int inf, int lan)
+        {
+            nazwisko = naz;
+            if (mat < 100 && mat > 0)
+                punktyMatematyka = mat;
+            else if (mat <= 0)
+                punktyMatematyka = 0;
+            else
+                punktyMatematyka = 100;
+            if (inf < 100 && inf > 0)
+                punktyInformatyka = mat;
+            else if (inf <= 0)
+                punktyInformatyka = 0;
+            else
+                punktyInformatyka = 100;
+            if (lan < 100 && lan > 0)
+                punktyJezykObcy = mat;
+            else if (lan <= 0)
+                punktyJezykObcy = 0;
+            else
+                punktyJezykObcy = 100;
+        }
+        public float Punkty(int mat, int inf, int lan)
+        {
+            return 0.6f * mat + 0.5f * inf + 0.2f * lan;
+        }
+    }
+}
